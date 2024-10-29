f685cba544f4f9b00792f742f99e838d5a2618bb
Art <pedroarteta@hotmail.es>
2024-10-29 00:46:51 -0500

    Refactor

--- src/app/main.py
+++
@@ -1,8 +0,0 @@
-import time
-
-from app import App
-
-if __name__ == "__main__":
-    app = App()
-    app.protocol("WM_DELETE_WINDOW", app.on_closing)  # Asegura que se realice limpieza al cerrar
-    app.mainloop()
